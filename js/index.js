2138cd3fccf24fa4a9ea6d6af563b41bf1248440
shayan__CodeKing <shayanmahmood234@gmail.com>
2022-07-12 21:39:36 +0500

    HELLO!!!!  this site is responsover t9  1180px so!!! commit NO 108

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(".menu-icon").click(function () {
+    $(".menu-con").toggleClass("vis");
+    $(".menu-icon").toggleClass("toggle");
+    $(".menu-icon-inner").toggleClass("toggle-bg");
+});
+
+
+$(".messenger-icon").click(function(){
+$(".messenger-con").toggleClass("vis");
+$(".messenger-icon").toggleClass("toggle");
+$(".messenger-icon-inner").toggleClass("toggle-bg");
+});
+
+$(".notification-icon").click(function(){
+    $(".notification-con").toggleClass("vis");
+    $(".notification-icon").toggleClass("toggle");
+    $(".notification-icon-inner").toggleClass("toggle-bg");
+});
+
+$(".profile-icon").click(function(){
+$(".profile-con").toggleClass("vis");
+$(".profile-icon").toggleClass("toggle");
+$(".profile-icon-inner").toggleClass("toggle-bg");
+});
+// messenger-icon
